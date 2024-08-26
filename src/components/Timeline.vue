96924a4d9caf57e99df41d26b86266cbc53d82b9
ahmadalanib <alnaib888@gmail.com>
2024-08-26 11:56:29 +0200

    add time line

---
+++ src/components/Timeline.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<nav class="bg-white shadow-md text-center py-4">
+<span class="text-blue-500">
+  <a >Today</a>
+  <a class="mx-5">This week</a>
+  <a>This month</a>
+</span>
+</nav>
+</template>
