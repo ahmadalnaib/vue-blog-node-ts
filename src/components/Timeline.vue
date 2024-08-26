353a64f6dc20efba0f7b5b47a9ebdd129399d6f5
ahmadalanib <alnaib888@gmail.com>
2024-08-26 13:31:08 +0200

    Add events and reactivity

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+const periods = ref(['Today', 'This week', 'This month']);
+
+const selectedPeriod = ref('Today');
+const selectPeriod = (period: string) => {
+  selectedPeriod.value = period;
+};
 </script>
 
 <template>
-<nav class="bg-white shadow-md text-center py-4">
-<span class="text-blue-500">
-  <a >Today</a>
-  <a class="mx-5">This week</a>
-  <a>This month</a>
-</span>
-</nav>
+  <nav class="bg-white shadow-md text-center py-4">
+    <span class="text-blue-500">
+      <a
+        class="mx-5"
+         :class="{ 'bg-blue-100 text-blue-700 p-2 rounded-md': selectedPeriod === period }"
+        v-for="period in periods"
+        :key="period"
+        @click="selectPeriod(period)"
+        >{{ period }}</a
+      >
+    </span>
+  </nav>
 </template>
